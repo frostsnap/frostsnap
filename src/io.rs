d5fd35d2fa2cb86b186ffbcdad32c6c03dd56982
Adam Mashrique <amashrique@gmail.com>
2023-02-10 18:28:13 +0800

    i2c master impl for DeviceIO

--- src/io.rs
+++ src/io.rs
@@ -2,6 +2,7 @@
 //! would be nice to have serial, i2c, http, all in the one `io/` directory.
 
 use esp_idf_hal::delay::BLOCK;
+use esp_idf_hal::i2c::I2cDriver;
 use esp_idf_hal::uart::{self, UartDriver};
 use std::fmt::Write;
 use std::thread;
@@ -47,7 +48,7 @@ pub fn read_str_from_serial(uart: &mut UartDriver) -> String {
     std::string::String::from_utf8(read_all_from_serial(uart)).expect("valid utf8")
 }
 
-impl DeviceIO for uart::UartDriver<'_> {
+impl DeviceIO for UartDriver<'_> {
     /// Read a [`FrostMessage`] from serial.
     ///
     /// Returns an option of a message. The read is flushed if an error occurs.
@@ -74,6 +75,57 @@ impl DeviceIO for uart::UartDriver<'_> {
     fn write_messages(&mut self, messages: Vec<FrostMessage>) {
         let write_bytes = bincode::serialize(&messages).unwrap();
         write_bytes_to_serial(self, &write_bytes);
+        thread::sleep(Duration::from_millis(1000));
+    }
+}
+
+// i2c
+pub fn read_from_i2c(i2c: &mut I2cDriver) -> Vec<u8> {
+    let mut buf = vec![0; 1024];
+    i2c.read(0x21, &mut buf.as_mut_slice(), 1000)
+        .unwrap_or_else(|e| eprintln!("Failed to read from i2c: {:?}", e));
+    buf
+}
+
+pub fn write_to_i2c(i2c: &mut I2cDriver, message: &Vec<u8>) {
+    i2c.write(0x21, &message[..], 1000).unwrap();
+}
+
+pub fn flush_i2c(i2c: &mut I2cDriver) {
+    let mut buf = [0_u8; 8];
+    let empty = [0xff; 7];
+    while buf[1..] != empty {
+        i2c.read(0x21, &mut buf, 2).unwrap();
+    }
+}
+
+impl DeviceIO for I2cDriver<'_> {
+    /// Read a [`FrostMessage`] from serial.
+    ///
+    /// Returns an option of a message. The read is flushed if an error occurs.
+    fn read_messages(&mut self) -> Vec<FrostMessage> {
+        let binding = read_from_i2c(self);
+        let received = binding.as_slice();
+        if received.len() > 0 {
+            match bincode::deserialize::<Vec<FrostMessage>>(received) {
+                Ok(messages) => {
+                    // println!("Read from serial:");
+                    // dbg!(&message);
+                    return messages;
+                }
+                Err(e) => {
+                    eprintln!("Error reading message: {:?}", e);
+                    flush_i2c(self);
+                    return vec![];
+                }
+            }
+        }
+        return vec![];
+    }
+
+    fn write_messages(&mut self, messages: Vec<FrostMessage>) {
+        let write_bytes = bincode::serialize(&messages).unwrap();
+        write_to_i2c(self, &write_bytes);
         thread::sleep(Duration::from_millis(1000));
     }
 }
