c1db9d052f3b14b33f6f2c92c946035f5e3b2de4
Nick Farrow <nick@nickfarrow.com>
2023-03-02 21:39:20 +1100

    Choice of serial port menu

--- coordinator-cli/src/main.rs
+++ coordinator-cli/src/main.rs
@@ -1,40 +1,38 @@
-use serde::{Deserialize, Serialize};
-use serde_json::{json, Value as JsonValue};
-use std::collections::BTreeMap;
+use serde_json::json;
+use std::error::Error;
 use std::io::{self, Write};
 use std::str;
 use std::thread::sleep;
 use std::time::Duration;
-use std::{error::Error, sync::Mutex};
 
-use schnorr_fun::frost::Nonce;
-use schnorr_fun::{frost::PointPoly, fun::Scalar, Signature};
+fn read_string() -> String {
+    let mut input = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("can not read user input");
+    let cleaned_input = input.trim().to_string();
+    cleaned_input
+}
 
-#[derive(Debug)]
-pub struct FrostDatabase {
-    threshold: usize,
-    n_parties: usize,
-    count: i32,
+fn fetch_input(prompt: &str) -> String {
+    println!("{}", prompt);
+    read_string()
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut i = 0;
-    let mut frost_db = FrostDatabase {
-        threshold: 2,
-        n_parties: 3,
-        count: i,
-    };
-    let mut found_port = String::new();
-    println!("Waiting for esp32");
-    loop {
+    println!("Finding serial devices...");
+    println!("");
+    let found_port: String = loop {
         let ports = serialport::available_ports()?;
-        println!("{:?}", ports);
-        if ports.len() > 0 {
-            found_port.push_str(ports[0].port_name.as_str());
-            break;
-        } 
-        sleep(Duration::from_millis(1000));
-    }
+        for (port_index, port) in ports.iter().enumerate() {
+            println!("{:?} -- {:?}", port_index, port);
+        }
+
+        match fetch_input("Type index or enter to refresh: ").parse::<usize>() {
+            Ok(index_selection) => break ports[index_selection].port_name.clone(),
+            Err(_) => {}
+        }
+    };
 
     println!("Connecting to {}", found_port);
     let mut port = serialport::new(&found_port, 115_200)
@@ -45,46 +43,50 @@ fn main() -> Result<(), Box<dyn Error>> {
             std::process::exit(1);
         });
     loop {
-        // let mut writebuf: Vec<u8> = vec![0; 1024];
-        // // write!(writebuf.as_mut_slice(), "{}", json!({ "success": true }).to_string())?;
-        // writebuf.as_mut_slice().write("fgsfds".as_bytes())?;
-        // match port.write(&writebuf.as_slice()) {
-        //     Ok(_t) => {
-        //         println!("to client: {}", str::from_utf8(&writebuf[..])?);
-        //         // break;
-        //     }
-        //     Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
-        //     Err(e) => eprintln!("{:?}", e),
-        // }
-
-        // sleep(Duration::from_millis(1000));
-
-        let mut readbuf: Vec<u8> = vec![0; 1024];
-        match port.read(readbuf.as_mut_slice()) {
-            Ok(t) => {
-                let req = str::from_utf8(&readbuf[..t - 1])?;
-                println!("from client: {}", req);
-                // let res = request_handler(req, &mut frost_db);
-
-                // let mut writebuf: Vec<u8> = vec![0; 1024];
-                // // write!(writebuf.as_mut_slice(), "{}", json!({ "success": true }).to_string())?;
-                // writebuf.as_mut_slice().write("fgsfds".as_bytes())?;
-                // match port.write(&writebuf.as_slice()) {
-                //     Ok(_t) => {
-                //         println!("to client: {}", str::from_utf8(&writebuf[..])?);
-                //         // break;
-                //     }
-                //     Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
-                //     Err(e) => eprintln!("{:?}", e),
-                // }
+        let mut writebuf: Vec<u8> = vec![0; 1024];
+        write!(
+            writebuf.as_mut_slice(),
+            "{}",
+            json!({ "success": true }).to_string()
+        )?;
+        writebuf.as_mut_slice().write("fgsfds".as_bytes())?;
+        match port.write(&writebuf.as_slice()) {
+            Ok(_t) => {
+                println!("to client: {}", str::from_utf8(&writebuf[..])?);
+                // break;
             }
             Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
-            Err(ref e) if e.kind() == io::ErrorKind::BrokenPipe => {
-                eprintln!("{} disconnected", &found_port);
-                std::process::exit(1);
-            }
             Err(e) => eprintln!("{:?}", e),
         }
+
+        sleep(Duration::from_millis(1000));
+
+        // let mut readbuf: Vec<u8> = vec![0; 1024];
+        // match port.read(readbuf.as_mut_slice()) {
+        //     Ok(t) => {
+        //         let req = str::from_utf8(&readbuf[..t - 1])?;
+        //         println!("from client: {}", req);
+        //         // let res = request_handler(req, &mut frost_db);
+
+        //         // let mut writebuf: Vec<u8> = vec![0; 1024];
+        //         // // write!(writebuf.as_mut_slice(), "{}", json!({ "success": true }).to_string())?;
+        //         // writebuf.as_mut_slice().write("fgsfds".as_bytes())?;
+        //         // match port.write(&writebuf.as_slice()) {
+        //         //     Ok(_t) => {
+        //         //         println!("to client: {}", str::from_utf8(&writebuf[..])?);
+        //         //         // break;
+        //         //     }
+        //         //     Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+        //         //     Err(e) => eprintln!("{:?}", e),
+        //         // }
+        //     }
+        //     Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+        //     Err(ref e) if e.kind() == io::ErrorKind::BrokenPipe => {
+        //         eprintln!("{} disconnected", &found_port);
+        //         std::process::exit(1);
+        //     }
+        //     Err(e) => eprintln!("{:?}", e),
+        // }
     }
 
     // Ok(())
