d9fa190932c8957c743bd42ab79c7c65f6d82de7
Nick Farrow <nick@nickfarrow.com>
2023-03-02 14:57:49 +1100

    Create clean slate for device build based on frostsnap_uart

---
+++ device/src/lib.rs
@@ -0,0 +1,7 @@
+use log::LevelFilter;
+
+pub const OUR_INDEX: usize = 0;
+pub const LOG_LEVEL: LevelFilter = LevelFilter::Debug;
+
+// pub const OUR_INDEX: usize = 2;
+// pub const LOG_LEVEL: LevelFilter = LevelFilter::Off;
