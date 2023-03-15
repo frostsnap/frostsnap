8abcd7178ac058feb964b1ed83a11e7db01f75d3
Nick Farrow <nick@nickfarrow.com>
2023-03-15 22:35:47 +1100

    UART buffer peek_read experiments

--- device/src/uart.rs
+++ device/src/uart.rs
@@ -1,5 +1,6 @@
 extern crate alloc;
 use alloc::format;
+use alloc::vec::Vec;
 use bincode::de::read::Reader;
 use bincode::enc::write::Writer;
 use bincode::error::DecodeError;
@@ -8,14 +9,21 @@ use esp32c3_hal::prelude::_embedded_hal_serial_Read;
 use esp32c3_hal::Uart;
 use esp_hal_common::uart::Instance;
 use esp_hal_common::{timer::Instance as TimerInstance, Timer};
+use esp_println::println;
+
 pub struct DeviceUart<'a, T, U> {
     pub uart: Uart<'a, T>,
+    from_buffer_cache: Vec<u8>,
     timer: Timer<U>,
 }
 
 impl<'a, T, U> DeviceUart<'a, T, U> {
     pub fn new(uart: Uart<'a, T>, timer: Timer<U>) -> Self {
-        Self { uart, timer }
+        Self {
+            uart,
+            timer,
+            from_buffer_cache: Vec::new(),
+        }
     }
 }
 
@@ -24,10 +32,44 @@ where
     T: Instance,
     U: TimerInstance,
 {
+    fn peek_read(&mut self, n_bytes: usize) -> Option<&[u8]> {
+        for _ in 0..n_bytes {
+            match self.uart.read() {
+                Err(_) => break,
+                Ok(c) => self.from_buffer_cache.push(c),
+            }
+        }
+
+        println!("Peeking read {:?}", self.from_buffer_cache);
+        return if self.from_buffer_cache.len() == 0 {
+            None
+        } else {
+            Some(self.from_buffer_cache.as_slice())
+        };
+    }
+
+    fn consume(&mut self, n_bytes: usize) {
+        println!("Consoooming");
+        if self.from_buffer_cache.len() <= n_bytes {
+            self.from_buffer_cache = Vec::new();
+        } else {
+            self.from_buffer_cache = self.from_buffer_cache[n_bytes..].to_vec()
+        }
+    }
+
     fn read(&mut self, bytes: &mut [u8]) -> Result<(), DecodeError> {
         let mut i = 0;
         let mut error_state = false;
         let mut start: u64 = 0;
+        println!("Normal read, existing cache: {:?}", self.from_buffer_cache);
+
+        // Take bytes from existing cached first
+        for byte in &self.from_buffer_cache {
+            bytes[i] = *byte;
+            i += 1
+        }
+        self.consume(i);
+
         while i < bytes.len() {
             match self.uart.read() {
                 Err(e) => {
@@ -37,7 +79,8 @@ where
                     }
                     let elapsed = (self.timer.now() - start) / 40000;
                     // timeout, return error after 100ms
-                    if elapsed > 100 {
+                    if true {
+                        //elapsed > 100 {
                         return Err(DecodeError::OtherString(format!("{:?}", e)));
                     }
                     // retries uart read
@@ -51,6 +94,7 @@ where
                 }
             };
         }
+
         Ok(())
     }
 }
