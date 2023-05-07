a4daad941dbbc7d4423a5f1da17f350e8fac1d80
Nick Farrow <nick@nickfarrow.com>
2023-05-08 03:06:28 +1000

    cargo fmt

--- frostsnap_core/src/xpub.rs
+++ frostsnap_core/src/xpub.rs
@@ -1,12 +1,11 @@
 use schnorr_fun::fun::Point;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Ord, PartialOrd, Hash)]
-pub struct ExtendedPubKey([u8;65]);
-
+pub struct ExtendedPubKey([u8; 65]);
 
 impl ExtendedPubKey {
-    pub fn new(public_key: Point, chain_code: [u8;32]) -> Self {
-        let mut key = [0u8;65];
+    pub fn new(public_key: Point, chain_code: [u8; 32]) -> Self {
+        let mut key = [0u8; 65];
         key[0..33].copy_from_slice(public_key.to_bytes().as_ref());
         key[33..65].copy_from_slice(&chain_code);
         Self(key)
