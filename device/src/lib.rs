f82e65579da11b1fded34f2e78fd0457a426fd5b
Adam Mashrique <amashrique@gmail.com>
2023-06-17 16:08:03 +0300

    Exclude unused display module

--- device/src/lib.rs
+++ device/src/lib.rs
@@ -6,7 +6,9 @@ extern crate alloc;
 pub mod buttons;
 pub mod device_config;
 pub mod io;
+#[cfg(feature = "purple")]
 pub mod oled;
+#[cfg(feature = "air101")]
 pub mod st7735;
 pub mod state;
 pub mod storage;
