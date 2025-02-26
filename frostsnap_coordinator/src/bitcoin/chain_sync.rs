e11f7a5d25015be9edb09907863ce7c9d37a4253
Lloyd Fournier <LLFourn@users.noreply.github.com>
2025-02-26 15:18:16 +1100

    [app] Use our electrum servers as default (#220)
    * [app] Use our electrum servers as default
    
    * Remove erroneous cargo patch for schnorr_fun

--- frostsnap_coordinator/src/bitcoin/chain_sync.rs
+++ frostsnap_coordinator/src/bitcoin/chain_sync.rs
@@ -141,11 +141,11 @@ impl ChainClient {
 
 pub const fn default_electrum_server(network: bitcoin::Network) -> &'static str {
     match network {
-        bitcoin::Network::Bitcoin => "ssl://electrum.blockstream.info:50002",
+        bitcoin::Network::Bitcoin => "tcp://electrum.frostsn.app:50001",
         // we're using the tcp:// version since ssl ain't working for some reason
         bitcoin::Network::Testnet => "tcp://electrum.blockstream.info:60001",
         bitcoin::Network::Regtest => "tcp://localhost:60401",
-        bitcoin::Network::Signet => "tcp://signet-electrumx.wakiyamap.dev:50001",
+        bitcoin::Network::Signet => "tcp://electrum.frostsn.app:60001",
         _ => panic!("Unknown network"),
     }
 }
