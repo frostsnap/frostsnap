b0bc7f6ee7979ca350d24031d4d065898ff22fcc
志宇 <hello@evanlinjin.me>
2025-03-12 09:49:37 +1100

    [app] Move psbt-specific stuff into `psbt.dart`

--- frostsnapp/lib/psbt.dart
+++ frostsnapp/lib/psbt.dart
@@ -12,7 +12,6 @@ import 'package:frostsnapp/settings.dart';
 import 'package:frostsnapp/sign_message.dart';
 import 'package:frostsnapp/snackbar.dart';
 import 'package:frostsnapp/wallet.dart';
-import 'package:frostsnapp/wallet_send.dart';
 import 'package:pretty_qr_code/pretty_qr_code.dart';
 import 'ffi.dart' if (dart.library.html) 'ffi_web.dart';
 
@@ -400,4 +399,154 @@ class _AnimatedQrState extends State<AnimatedQr> {
       ],
     );
   }
+}
+
+class EffectTable extends StatelessWidget {
+  final EffectOfTx effect;
+  const EffectTable({super.key, required this.effect});
+
+  @override
+  Widget build(BuildContext context) {
+    List<TableRow> transactionRows =
+        effect.foreignReceivingAddresses.map((entry) {
+          final (address, value) = entry;
+          return TableRow(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Send to $address'),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SatoshiText.withSign(value: -value),
+              ),
+            ],
+          );
+        }).toList();
+
+    transactionRows.add(
+      TableRow(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child:
+                effect.feerate != null
+                    ? Text("${effect.feerate!.toStringAsFixed(1)} (sats/vb))")
+                    : Text("unknown"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SatoshiText.withSign(value: -effect.fee),
+          ),
+        ],
+      ),
+    );
+
+    transactionRows.add(
+      TableRow(
+        children: [
+          Padding(padding: const EdgeInsets.all(8.0), child: Text('Net value')),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SatoshiText.withSign(value: effect.netValue),
+          ),
+        ],
+      ),
+    );
+
+    final effectTable = Table(
+      columnWidths: const {0: FlexColumnWidth(4), 1: FlexColumnWidth(2)},
+      border: TableBorder.all(),
+      children: transactionRows,
+    );
+
+    final effectWidget = Column(
+      children: [describeEffect(context, effect), Divider(), effectTable],
+    );
+
+    return effectWidget;
+  }
+}
+
+Widget describeEffect(BuildContext context, EffectOfTx effect) {
+  final style = DefaultTextStyle.of(
+    context,
+  ).style.copyWith(fontWeight: FontWeight.w600);
+  final Widget description;
+
+  if (effect.foreignReceivingAddresses.length == 1) {
+    final (dest, amount) = effect.foreignReceivingAddresses[0];
+    description = Wrap(
+      direction: Axis.horizontal,
+      children: <Widget>[
+        Text('Sending '),
+        SatoshiText(value: amount, style: style),
+        Text(' to '),
+        Text(dest, style: style),
+      ],
+    );
+  } else if (effect.foreignReceivingAddresses.isEmpty) {
+    description = Text("Internal transfer");
+  } else {
+    description = Text("cannot describe this yet");
+  }
+
+  return description;
+}
+
+Future<bool> showBroadcastConfirmDialog(
+  BuildContext context, {
+  required MasterAppkey masterAppkey,
+  required SignedTx tx,
+  required SuperWallet superWallet,
+}) async {
+  final wasBroadcast = await showDialog<bool>(
+    context: context,
+    barrierDismissible: false,
+    builder: (dialogContext) {
+      final effect = tx.effect(
+        masterAppkey: masterAppkey,
+        network: superWallet.network,
+      );
+      final effectWidget = EffectTable(effect: effect);
+      return AlertDialog(
+        title: Text("Broadcast?"),
+        content: SizedBox(
+          width: Platform.isAndroid ? double.maxFinite : 400.0,
+          child: Align(alignment: Alignment.center, child: effectWidget),
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              if (dialogContext.mounted) {
+                Navigator.pop(dialogContext, false);
+              }
+            },
+            child: Text("Cancel"),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              try {
+                await superWallet.broadcastTx(
+                  masterAppkey: masterAppkey,
+                  tx: tx,
+                );
+                if (dialogContext.mounted) {
+                  Navigator.pop(context, true);
+                }
+              } catch (e) {
+                if (dialogContext.mounted) {
+                  Navigator.pop(dialogContext, false);
+                  showErrorSnackbarTop(dialogContext, "Broadcast error: $e");
+                }
+              }
+            },
+            child: Text("Broadcast"),
+          ),
+        ],
+      );
+    },
+  );
+
+  return wasBroadcast ?? false;
 }
