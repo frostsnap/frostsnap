414b28be11fb503f8b43c3a29f0dc89c49986eb8
Nick Farrow <nick@nickfarrow.com>
2025-07-16 23:48:02 +1000

    [factory, device] cleanup and sign challenge at end

--- device/src/flash/genuine_certificate.rs
+++ device/src/flash/genuine_certificate.rs
@@ -2,7 +2,7 @@ use alloc::vec::Vec;
 use frostsnap_comms::factory::Certificate;
 use frostsnap_core::Versioned;
 
-#[derive(Debug, Clone, bincode::Encode, bincode::Decode)]
+#[derive(Debug, Clone, bincode::Encode, bincode::Decode, PartialEq)]
 pub struct FactoryData {
     inner: Versioned<FactoryDataContents>,
 }
@@ -16,9 +16,15 @@ impl FactoryData {
             }),
         }
     }
+
+    pub fn encrypted_params(&self) -> Vec<u8> {
+        match &self.inner {
+            Versioned::V0(contents) => contents.encrypted_params.clone(),
+        }
+    }
 }
 
-#[derive(Debug, Clone, bincode::Encode, bincode::Decode)]
+#[derive(Debug, Clone, PartialEq, bincode::Encode, bincode::Decode)]
 pub struct FactoryDataContents {
     pub encrypted_params: Vec<u8>,
     pub certificate: Certificate,
