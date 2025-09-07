a400a86799876b30b4e042f4bc3aa02fd04d85e0
Nick Farrow <nick@nickfarrow.com>
2025-09-08 00:45:16 +1000

    [factory] fix repeated genuine checks

--- frostsnap_factory/src/db.rs
+++ frostsnap_factory/src/db.rs
@@ -76,15 +76,22 @@ impl Database {
         firmware_digest: Sha256Digest,
     ) -> Result<(), Box<dyn std::error::Error>> {
         let mut conn = self.get_conn()?;
-        let result = conn.exec_iter(
+
+        let exists: Option<u8> = conn.exec_first(
+            "SELECT 1 FROM devices WHERE serial_number = ?",
+            (serial_number,),
+        )?;
+
+        if exists.is_none() {
+            return Err(format!("Serial number {} not found in database", serial_number).into());
+        }
+
+        // Allow genuine checks to succeed again
+        conn.exec_drop(
             "UPDATE devices SET firmware_hash = ?, genuine_verified = TRUE, status = 'genuine_verified' 
              WHERE serial_number = ?",
             (firmware_digest.to_string(), serial_number),
         )?;
-
-        if result.affected_rows() == 0 {
-            return Err(format!("Serial number {} not found in database", serial_number).into());
-        }
 
         Ok(())
     }
