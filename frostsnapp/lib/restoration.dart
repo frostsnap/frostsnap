33bc80d91e57d845ce9d22da41beeb5eea10b059
LLFourn <lloyd.fourn@gmail.com>
2025-10-23 15:36:53 +1100

    [app,coord] New restoration flow
    Prompt for a device at the start. And use it the whole way through.
    The kind of device determines what flow we'll do.

--- frostsnapp/lib/restoration.dart
+++ frostsnapp/lib/restoration.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:frostsnap/contexts.dart';
 import 'package:frostsnap/global.dart';
 import 'package:frostsnap/maybe_fullscreen_dialog.dart';
-import 'package:frostsnap/restoration/recovery_flow.dart';
+import 'package:frostsnap/restoration/device_discovery.dart';
 import 'package:frostsnap/restoration/state.dart';
 import 'package:frostsnap/src/rust/api.dart';
 
 export 'package:frostsnap/restoration/recovery_flow.dart'
     show WalletRecoveryFlow;
+export 'package:frostsnap/restoration/device_discovery.dart'
+    show RecoveryFlowWithDiscovery;
 export 'package:frostsnap/restoration/state.dart';
 
 void continueWalletRecoveryFlowDialog(
@@ -15,21 +17,17 @@ void continueWalletRecoveryFlowDialog(
   required RestorationId restorationId,
 }) async {
   final homeCtx = HomeContext.of(context);
-  final restoration = coord.getRestorationState(restorationId: restorationId)!;
+
+  final recoveryContext = RecoveryContext.continuingRestoration(
+    restorationId: restorationId,
+  );
 
   await MaybeFullscreenDialog.show(
     context: context,
     barrierDismissible: true,
-    child: WalletRecoveryFlow(
-      recoveryContext: RecoveryContext.continuingRestoration(
-        restorationId: restorationId,
-        walletName: restoration.keyName,
-        network: restoration.keyPurpose.bitcoinNetwork()!,
-        threshold: restoration.accessStructure.effectiveThreshold(),
-      ),
-      isDialog: false,
-    ),
+    child: RecoveryFlowWithDiscovery(recoveryContext: recoveryContext),
   );
+
   await coord.cancelProtocol();
   if (homeCtx == null) {
     return;
