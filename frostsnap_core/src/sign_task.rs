bcf37647b0fea693b1bc6487fc8e1adbd29a2ab9
LLFourn <lloyd.fourn@gmail.com>
2026-08-03 12:16:55 +1000

    [core] Check output owners

--- frostsnap_core/src/sign_task.rs
+++ frostsnap_core/src/sign_task.rs
@@ -60,20 +60,25 @@ impl WireSignTask {
                     KeyPurpose::Bitcoin(network) => network,
                     _ => return Err(SignTaskError::WrongPurpose),
                 };
-                let non_matching_key = tx_template.inputs().iter().find_map(|input| {
-                    let owner = input.owner().local_owner()?;
-                    if owner.master_appkey != master_appkey {
-                        Some(owner.master_appkey)
-                    } else {
-                        None
-                    }
-                });
+                let input_owners = tx_template
+                    .inputs()
+                    .iter()
+                    .filter_map(|input| Some((input.owner().local_owner()?, "input")));
+                let output_owners = tx_template
+                    .outputs()
+                    .iter()
+                    .filter_map(|output| Some((output.owner().local_owner()?, "output")));
 
-                if let Some(non_matching_key) = non_matching_key {
-                    return Err(SignTaskError::WrongKey {
-                        got: Box::new(non_matching_key),
-                        expected: Box::new(master_appkey),
-                    });
+                let owners = input_owners.chain(output_owners);
+
+                for (owner, kind) in owners {
+                    if owner.master_appkey != master_appkey {
+                        return Err(SignTaskError::WrongKey {
+                            got: Box::new(owner.master_appkey),
+                            expected: Box::new(master_appkey),
+                            kind,
+                        });
+                    }
                 }
 
                 if !tx_template.has_any_inputs_to_sign() {
@@ -171,6 +176,7 @@ pub enum SignTaskError {
     WrongKey {
         got: Box<MasterAppkey>,
         expected: Box<MasterAppkey>,
+        kind: &'static str,
     },
     WrongPurpose,
     InvalidBitcoinTransaction,
@@ -180,9 +186,13 @@ pub enum SignTaskError {
 impl core::fmt::Display for SignTaskError {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         match self {
-            SignTaskError::WrongKey { got, expected } => write!(
+            SignTaskError::WrongKey {
+                got,
+                expected,
+                kind,
+            } => write!(
                 f,
-                "sign task was for key {expected} but got an item for key {got}",
+                "sign task was for key {expected} but got an {kind} for key {got}",
             ),
             SignTaskError::InvalidBitcoinTransaction => {
                 write!(f, "Bitcoin transaction input value was less than outputs")
